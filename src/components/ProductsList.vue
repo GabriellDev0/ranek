8ebef8259d8c8848ead565941a7c9ffc7981cbf4
GabriellDev0 <gabrielldev0@gmail.com>
2022-05-02 16:00:12 -0300

    Adicionei a biblioteca axios, para facilitar nosso desenvolvimento

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,10 +1,40 @@
 <template>
-        <p>Lista de Produtos</p>
+    <section class="products-container">
+           <div v-for="product in products" :key="product.id">
+
+              <img v-if="product.photos" :src="product.photos[0].src" :alt="product.photos[0].title">
+
+              <p class="price">{{product.price}}</p>
+
+              <h2 class="title">{{product.name}}</h2>
+
+              <p>{{product.description}}</p>
+           </div> 
+    </section>
 </template>
 
 <script>
+import { api } from "@/services.js";
 export default {
-    name: "ProductsList"
+    name: "ProductsList",
+    data(){
+        return{
+            products: null
+        }
+    },
+    methods: {
+        getProducts(){
+
+            api.get("/product").then(response =>{
+                this.products = response.data;
+            })
+
+            
+        }
+    },
+    created(){
+        this.getProducts()
+    }
 }
 </script>
 
--- src/components/ProductsSearch.vue
+++ src/components/ProductsSearch.vue
@@ -1,38 +1,10 @@
 <template>
-    <section class="products-container">
-           <div v-for="product in products" :key="product.id">
-
-              <img v-if="product.photos" :src="product.photos[0].src" :alt="product.photos[0].title">
-
-              <p class="price">{{product.price}}</p>
-
-              <h2 class="title">{{product.name}}</h2>
-
-              <p>{{product.description}}</p>
-           </div> 
-    </section>
+        <p>Procurar Produtos</p>
 </template>
 
 <script>
 export default {
-    name: "ProductsSearch",
-    data(){
-        return{
-            products: null
-        }
-    },
-    methods: {
-       async getProducts(){
-            const link = `http://localhost:3000/product`
-            const response = await fetch(link)
-            const responseJSON = await response.json()
-
-            return this.products = responseJSON
-        }
-    },
-    created(){
-        this.getProducts()
-    }
+    name: "ProductsSearch"
 }
 </script>
 
