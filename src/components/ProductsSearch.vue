39ceecf389f3343b447de0036f145b72ccfcb887
GabriellDev0 <gabrielldev0@gmail.com>
2022-05-02 15:28:26 -0300

    Adicionei a página de produtos e o fetch das informações através de uma api-localhost

--- src/components/ProductsSearch.vue
+++ src/components/ProductsSearch.vue
@@ -1,10 +1,38 @@
 <template>
-    <p>Busca</p>
+    <section class="products-container">
+           <div v-for="product in products" :key="product.id">
+
+              <img v-if="product.photos" :src="product.photos[0].src" :alt="product.photos[0].title">
+
+              <p class="price">{{product.price}}</p>
+
+              <h2 class="title">{{product.name}}</h2>
+
+              <p>{{product.description}}</p>
+           </div> 
+    </section>
 </template>
 
 <script>
 export default {
-    name: "ProductsSearch"
+    name: "ProductsSearch",
+    data(){
+        return{
+            products: null
+        }
+    },
+    methods: {
+       async getProducts(){
+            const link = `http://localhost:3000/product`
+            const response = await fetch(link)
+            const responseJSON = await response.json()
+
+            return this.products = responseJSON
+        }
+    },
+    created(){
+        this.getProducts()
+    }
 }
 </script>
 
