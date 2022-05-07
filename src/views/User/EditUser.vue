21dc34fcc881d46eb4d2bf6c1dc80ca732f4c92a
GabriellDev0 <gabrielldev0@gmail.com>
2022-05-07 13:45:26 -0300

    Adicionei a página de fazer alterações cadastrais

--- src/views/User/EditUser.vue
+++ src/views/User/EditUser.vue
@@ -1,10 +1,29 @@
 <template>
-    <p>Usuario Editar</p>
+    <section>
+        <UserForm>
+            <button class="btn" @click.prevent="updateUser">Atualizar Cadastro</button>
+        </UserForm>
+    </section>
 </template>
 
 <script>
+import UserForm from "@/components/UserForm.vue"
+import { api } from "@/services.js"
 export default {
-
+    name: "EditUser",
+    components:{
+        UserForm
+    },
+    methods:{
+        updateUser(){
+            api.put(`/user/${this.$store.state.user.id}`, this.$store.state.user).then(()=>{
+                this.$store.dispatch("getUserProducts");
+                this.$router.push({name: "user"})
+            }).catch((error)=>{
+                console.log(error.response)
+            })
+        }
+    }
 }
 </script>
 
